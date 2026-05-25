616b0b44fc3ec4d7f3f0e2f625d518dc67a0c8f5
Samuel Meyisso <samuelmeyisso635@gmail.com>
2026-05-26 00:20:18 +0200

    feat: add redirections for FLSH Limoges and Lycée Cours Lumière

--- src/App.jsx
+++ src/App.jsx
@@ -156,13 +156,15 @@ const INIT = {
       "id": "f2",
       "school": "F.L.S.H Limoges",
       "period": "Sept 2024 – Juil 2025",
-      "diploma": "L1 Langues Étrangères Appliquées (EN/G/ESP/ITAL)"
+      "diploma": "L1 Langues Étrangères Appliquées (EN/G/ESP/ITAL)",
+      "link": "https://www.flsh.unilim.fr/"
     },
     {
       "id": "f3",
       "school": "Lycée International Cours Lumière",
       "period": "Sept 2023 – Juil 2024",
-      "diploma": "Baccalauréat Général — Ses, LLcer"
+      "diploma": "Baccalauréat Général — Ses, LLcer",
+      "link": "https://www.courslumiere.org/"
     }
   ],
   "contact": [
